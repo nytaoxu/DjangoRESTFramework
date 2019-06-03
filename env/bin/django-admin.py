7632883ca35d867f4f2307b6ad866bb4fa0de8f7
Tao <nytaoxu@gmail.com>
2019-06-03 16:10:51 -0400

    Django REST Framework

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/william/Downloads/Python_Training/4th_week/06032019/tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
